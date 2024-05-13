dad7ab7dbeef2b741608e43703c9315ceffaac4e
Dev <s.silvac@outlook.com>
2024-05-13 17:59:15 -0300

    inicio de estilo da pagina de serviço para usuario

--- paginaDeServicosParaUsr.php
+++ paginaDeServicosParaUsr.php
@@ -10,72 +10,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Serviços Contratados</title>
-    <!-- Adicione links para CSS ou Bootstrap aqui, se necessário -->
-    <style>
-        .content {
-            height: 100%;
-            width: 100%;
-        }
-
-        .navServices {
-            margin-top: 5%;
-            font-size: 32px;
-            display: flex;
-            justify-content: space-around;
-        }
-
-        .navServices li {
-            font-size: 30px;
-            list-style: none;
-        }
-
-        .opa {
-            background-color: #38a4ac;
-            color: black;
-            padding: 20px;
-            border: 5px solid rgb(19, 74, 71);
-            border-radius: 5px;
-            font-size: 22px;
-            font-weight: bold;
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            width: 30%;
-            height: 10%;
-            text-align: center;
-            display
-        }
-
-        .closeButton {
-            background-color: transparent;
-            border: none;
-            cursor: pointer;
-            font-size: 18px;
-            color: #155724;
-            font-weight: bold;
-        }
-
-        .closeButton:hover {
-            scale: 1.2;
-            text-decoration: underline;
-            transition: 0.3s;
-        }
-
-        .semServicos {
-            height: 900px !important;
-            width: 100vh;
-            align-items: center;
-
-            margin: auto;
-        }
-        .semServicos h1 {
-           position: relative;
-           top: 45%;
-           left: 50%;
-           transform: translate(-50%, 0);
-        }
-    </style>
+    <link rel="stylesheet" href="./css/paginasDeServicoUsr.css">
+   
 </head>
 
 <body>
@@ -83,7 +19,6 @@
     include_once("connect.php");
     include_once("navUsr.php");
     ?>
-    <h1>Serviços Contratados</h1>
     <ul class="navServices">
         <li><a href="#" onclick="loadPage('servicosEmAndamento.php')">Servicos em Andamento</a></li>
         <li><a href="#" onclick="loadPage('servicosRealizadosUsr.php')">Serviços realizados</a></li>
--- servicosRealizadosUsr.php
+++ servicosRealizadosUsr.php
@@ -1,10 +1,11 @@
 <?php
-        include_once("connect.php");
-        session_start();
-        $id_Usr = $_SESSION['id_Usr'];
-        ?>
-    <h2>Serviços Realizados</h2>
-    <ul>
+include_once ("connect.php");
+session_start();
+$id_Usr = $_SESSION['id_Usr'];
+?>
+<div class="realizados">
+
+    <div class="realizado">
         <?php
         $sql = "SELECT sr.*, s.nomeService, s.descService, c.valor, c.data_Contratacao, cat.nome AS nomeCategoria
                 FROM servicos_realizados sr
@@ -14,7 +15,7 @@
                 WHERE sr.id_Usr = $id_Usr";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
+            while ($row = $result->fetch_assoc()) {
                 $valor = $row['valor'];
                 $valor = substr($valor, 0, -2);
                 $valor = number_format($valor, 2, ',', '.');
@@ -22,19 +23,30 @@
                 $data_contratacao = date('d/m/Y', strtotime($data_contratacao));
                 $data_realizacao = $row['data_realizacao'];
                 $data_realizacao = date('d/m/Y', strtotime($data_realizacao));
-                echo "<li>";
-                echo "Serviço: " . $row["nomeService"]. "<br>";
-                echo "Descrição: " . $row["descService"]. "<br>";
-                echo "Categoria: " . $row["nomeCategoria"]. "<br>";
-                echo "Valor: R$ $valor <br>";
+                echo "<label class='lbl_realizados'>Serviço:</label>";
+                echo "<p class='dados_realizados'>" . $row["nomeService"] . "</p>";
+                echo "<label class='lbl_realizados'>Descrição: </label>";
+
+                echo "<p class='dados_realizados>" . $row["descService"] . "</p>";
+                echo "<label class='lbl_realizados'>Categoria: </label>";
+
+                echo "<p class='dados_realizados>" . $row["nomeCategoria"] . "</p>";
+                echo "<label class='lbl_realizados'>Valor: </label>";
+
+                echo "<p class='dados_realizados>R$ $valor </p>";
                 // Se houver descrição de contratação, descomente a linha abaixo
-                // echo "Descrição da Contratação: " . $row["mensagem_cliente"]. "<br>";
-                echo "Data de Contratação: $data_contratacao <br>";
-                echo "Data de Realização: $data_realizacao <br>";
-                echo "</li>";
+                // echo "Descrição da Contratação: " . $row["mensagem_cliente"]. "</p>";
+                echo "<label class='lbl_realizados'>Data de Contratação:</label>";
+              
+                echo "<p class='dados_realizados> $data_contratacao </p>";
+                echo "<label class='lbl_realizados'>Data de Realização:</label>";
+              
+                echo "<p class='dados_realizados> $data_realizacao </p>";
             }
+            echo "</div>";
         } else {
             echo "<li>Nenhum serviço realizado.</li>";
         }
         $conn->close();
         ?>
+    </div>
