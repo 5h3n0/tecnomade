c6420d8e8aa9c320a1a119b3e0a81c9a2dbe73e6
Dev <s.silvac@outlook.com>
2024-05-24 17:46:44 -0300

    estilos na página de avaliação

--- avaliar.php
+++ avaliar.php
@@ -1,54 +1,56 @@
 <?php
-if(!isset($_SESSION)) 
-{ 
-    session_start(); 
-} 
+include_once ('connect.php');
+if (!isset($_SESSION)) {
+    session_start();
+}
 $id_Pf = $_SESSION['id_Pf'];
 header('Content-Type: text/html; charset=utf-8');
 unset($_SESSION['notification']);
 
+$sql = "SELECT * FROM prof WHERE id_Pf = $id_Pf";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $pfName = $row['pfName'];
+        $imgPf = $row['imgName'];
+        $descPf = $row['descPf'];
+    }
+    if ($imgPf != null) {
+        $imgPf = "upload/" . $imgPf;
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="uft-8">
     <title>Avaliação por Estrelas</title>
-    <style>
-        .star-rating {
-            direction: rtl;
-            display: inline-block;
-            padding: 20px;
-        }
-        .star-rating input[type="radio"] {
-            display: none;
-        }
-        .star-rating label {
-            font-size: 30px;
-            color: #ddd;
-            cursor: pointer;
-        }
-        .star-rating input[type="radio"]:checked ~ label {
-            color: #f2b600;
-        }
-        .star-rating label:hover,
-        .star-rating label:hover ~ label {
-            color: #f2b600;
-        }
-    </style>
+    <link rel="stylesheet" href="./css/avaliar.css">
 </head>
+
 <body>
-    <a href="avaliar.php">avaliar</a>
-    <a href="media.php"> media</a>
-    <form action="avaliacaoBd.php" method="post">
-        <div class="star-rating">
-            <input type="radio" id="star5" name="estrelas" value="5"><label for="star5">☆</label>
-            <input type="radio" id="star4" name="estrelas" value="4"><label for="star4">☆</label>
-            <input type="radio" id="star3" name="estrelas" value="3"><label for="star3">☆</label>
-            <input type="radio" id="star2" name="estrelas" value="2"><label for="star2">☆</label>
-            <input type="radio" id="star1" name="estrelas" value="1"><label for="star1">☆</label>
-        </div>
-        <input type="hidden" name="id_Pf" value="<?php echo $id_Pf ?>"> <!-- Id do produto a ser avaliado -->
-        <button type="submit">Avaliar</button>
-    </form>
+    <section class="avaliar">
+            <div class="dados_pf_avaliar">
+                <div id="img_avaliar">
+                <img src="<?php echo $imgPf ?>" alt="">
+                </div>
+                <p class="p_avaliar name"><?php echo $pfName ?></p>
+                <p class="p_avaliar"><?php echo $descPf ?></p>
+            </div>
+            <form action="avaliacaoBd.php" class="form_avaliar" method="post">
+                <div class="star-rating">
+                    <input type="radio" id="star5" name="estrelas" value="5"><label for="star5">&#9733;</label>
+                    <input type="radio" id="star4" name="estrelas" value="4"><label for="star4">&#9733;</label>
+                    <input type="radio" id="star3" name="estrelas" value="3"><label for="star3">&#9733;</label>
+                    <input type="radio" id="star2" name="estrelas" value="2"><label for="star2">&#9733;</label>
+                    <input type="radio" id="star1" name="estrelas" value="1"><label for="star1">&#9733;</label>
+                </div>
+                <input type="hidden" name="id_Pf" value="<?php echo $id_Pf ?>"> <!-- Id do produto a ser avaliado -->
+                <button type="submit" class="btn-avaliar">Avaliar</button>
+            </form>
+    </section>
 </body>
+
 </html>
